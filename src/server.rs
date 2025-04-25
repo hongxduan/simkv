2e18ce9ab19ab73ba95baf1071b05d8d0ae401eb
Hongx <hongdxing@gmail.com>
2025-04-25 08:02:15 +0800

    add server config

--- src/server.rs
+++ src/server.rs
@@ -4,12 +4,13 @@
 //! date: 4 Apr, 2025
 //!
 
-use raft::raft::Raft;
+use clap::Parser;
+use config_file::FromConfigFile;
+use serde::Deserialize;
 use tokio::net::TcpListener;
 use tokio::signal;
 
 use std::env;
-use std::sync::{Arc, Mutex};
 
 mod db;
 
@@ -20,23 +21,41 @@ mod runner;
 
 mod raft;
 
+#[derive(Parser, Debug)]
+#[command(version, about, disable_help_flag=true, long_about = None)]
+struct Args {
+    #[arg(short,long, default_value_t=String::from("server.toml"))]
+    config: String,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    server: Server,
+}
+
+#[derive(Deserialize)]
+struct Server {
+    host: String,
+    port: u16,
+}
+
 /// SimKV server main fn
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    run().await
+    let args = Args::parse();
+    let config = Config::from_config_file(args.config).unwrap();
+
+    run(config).await
 }
 
 /// Run
-async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8303".to_string());
-
+async fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     tokio::spawn(async {
         let _ = crate::raft::raft::Raft::start().await;
     });
 
-    let listener = TcpListener::bind("0.0.0.0:8303").await?;
+    let addr = format!("{}:{}", config.server.host, config.server.port);
+    let listener = TcpListener::bind(addr.clone()).await?;
     println!("Listening on: {}", addr);
 
     runner::run(listener, signal::ctrl_c()).await;
